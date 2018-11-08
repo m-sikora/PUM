e073ed88b7a5530f4cfbc350d9c0fb632426575b
m-sikora <msikora08@gmail.com>
2018-11-08 22:48:05 +0100

    e

--- zad3b/scripts/rbf_pca.py
+++ zad3b/scripts/rbf_pca.py
@@ -8,7 +8,7 @@ points = read_data('../data/PCA.png')
 df = pd.DataFrame(points)
 x = df.loc[:, list(range(2))]
 y = df.loc[:, [2]]
-kpca = KernelPCA(kernel="rbf", n_components=2)
+kpca = KernelPCA(kernel="rbf", n_components=2, gamma=9000)
 mapped_points = kpca.fit_transform(x)
 mapped_df = pd.DataFrame(mapped_points)
 final_df = pd.concat([mapped_df, y], axis=1)
--- zad3b/src/visualise.py
+++ zad3b/src/visualise.py
@@ -4,6 +4,6 @@ import matplotlib.pyplot as plt
 def v2d(df):
     for x, y, c in df.values.tolist():
         plt.plot([x], [y], marker='o', markersize=1, color=c)
-    plt.quiver(0, 0, 1, 0)
-    plt.quiver(0, 0, 0, 1)
+    # plt.quiver(0, 0, 1, 0)
+    # plt.quiver(0, 0, 0, 1)
     plt.show(block=True)
